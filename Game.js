dfc148c6293f857336944fcfa28b62d4a5be0aa2
Samad <samadafzal1@g.ucla.edu>
2022-10-15 19:42:24 -0700

    created player function

--- Game.js
+++ Game.js
@@ -28,6 +28,31 @@ function createPlayers(num){
         players.push(player);
     }
 }
+//needed major help. too confusing. should be creating the players.//
+function createPlayersUI() {
+    document.getElementById('players').innerHTML = '';
+    for(let i = 0; i < players.length; i++){
+
+        var div_player = document.createElement('div');
+        var div_playerid = document.createElement('div');
+        var div_hand = document.createElement('div');
+        var div_points = document.createElement('div')
+
+        div_points.className = 'points';
+        div_points.id = 'points_' + i;
+        div_player.id = 'player_' + i;
+        div_player.className = 'player';
+        div_hand.id = 'hand_' + i;
+
+        div_playerid.innerHTML = 'Player' + players[i].id;
+        div_player.appendChild(div_playerid);
+        div_player.appendChild(div_hand);
+        div_player.appendChild(div_points);
+        document.getElementById('players').appendChild(div_player);
+
+
+    }
+}
 // need to adjust shuffle - not sure how to code it//
 function shuffle(){
     for(let i = deck.length - 1; i > 0; i--) {
